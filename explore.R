e4ebd1875f49e0027ef4a099d013cd4b68c6c79d
Dmitry Grekov <Krokodim@users.noreply.github.com>
2015-03-13 13:24:14 +0300

    Update explore.R

--- explore.R
+++ explore.R
@@ -1,37 +1,50 @@
-library(dplyr)
-library(lubridate)
+library(dplyr)      # a Grammar of data manipulation
+library(lubridate)  # make dealing with dates a little easier
+library(ggplot2)    # an implementation of the Grammar of Graphics
 
-dt <-read.csv("activity.csv", stringsAsFactors=FALSE) %>%
-  mutate(
-      interval      = sprintf("%04d", interval),
-      char.datetime = sprintf("%s %s", date, interval),
-      datetime      = ymd_hm(char.datetime),
-      hour          = sprintf("%02d", hour(datetime))
-    )                                                 %>%
-  select (date, interval, hour, steps)
+dt <-read.csv("activity.csv", stringsAsFactors=FALSE)
+
+dt <- mutate(dt, date = ymd(date))
+
+dt <- mutate(
+  dt, 
+  #step 1 - make it 'hhmm'
+  interval = sprintf("%04d", interval),
+  #step 2 - split hours and minutes with a colon
+  interval = paste(substr(interval,1,2),substr(interval,3,4), sep=":")
+)
+
+dt <- mutate(
+  dt,
+  hour     =    paste(substr(interval,1,2),"00", sep=":"),
+  day.type =    factor(
+    ifelse(wday(date) %in% 2:6, 1, 2), 
+    levels = c(1,2),
+    labels = c("weekday","weekend")
+  )
+)
 
 
+dt.daily <- dt                     %>%
+  na.omit()                        %>%
+  group_by(date)                   %>%
+  summarise(value=sum(steps)) 
 
-dt$date.time <- ymd_hm(paste(dt.raw$date, sprintf("%04d",dt.raw$interval)))
-dt$date <- ymd
-dt$interval <- NULL
+# build the plot
+g1 <- ggplot(dt.daily, aes(x=value)) 
 
-  
-  
-  dt.raw$date +  minutes(dt.raw$interval)
-dt.raw$hour <- hour(dt.raw$date.time) 
+# draw the histogram
+g1 <- g1 + geom_histogram(binwidth=1000, fill="steelblue", color="white", alpha=7/13)
 
-dt.raw$hour <- hour(dt.raw$date+minutes(interval) )
+# X-axis adjustments
+g1 <- g1 + xlab("Number of steps per day") 
+g1 <- g1 + scale_x_continuous(breaks=seq(0,21000, by=1000), limits=c(0,21000)) 
+g1 <- g1 + theme(axis.text.x = element_text(angle=60, hjust= 1))
 
-unique(dt.raw$date)
+# Y-label adjustments
+g1 <- g1 + ylab("Number of days")
+g1 <- g1 + scale_y_continuous(breaks=0:30) 
 
-dt.timely.total <- dt.raw         %>%
-  na.omit()                       %>%
-  group_by(interval)              %>%
-  summarise(value=sum(steps))     %>%
-  select(interval,value)          %>%
-  na.omit()      
-
-str(dt.timely.total)
+print(g1)
 
 
